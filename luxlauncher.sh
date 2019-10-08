893557edcb15708df0c3b2968a63820bb688448c
BuildTools <unconfigured@null.spigotmc.org>
2019-10-08 09:48:24 +0100

    update

---
+++ luxlauncher.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ttlauncher.sh
+# Navigate to sub-directory in home directory
+
+cd /home/pi/
+python3 Lux0.03.py &
---
+++ ttlauncher2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ttlauncher.sh
+# Navigate to sub-directory in home directory
+
+cd /home/pi/
+python3 twilightswitch_1.1.py &
